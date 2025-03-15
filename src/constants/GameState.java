4f35efb2e22df2d7bbc34f13563f5c5b75e6e4a6
Adijans Juris <juris.adijans@ziklo.com>
2025-03-15 16:25:27 +0100

    Add constants

---
+++ src/constants/GameState.java
@@ -0,0 +1,9 @@
+package constants;
+
+public enum GameState {
+	
+	COMPLETE,
+	ACTIVE,
+	NEW;
+
+}
---
+++ src/constants/Messages.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Messages {
+	
+	public static final String GAME_COMPLETE = "Congrats! New Game?";
+	
+	public static final String ERROR = "An error has occured ";
+
+
+}
